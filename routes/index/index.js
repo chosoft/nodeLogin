63a009aa5b87ef337ebae13b99c018127ea6634e
chosoft <creadorcuentasmolina@gmail.com>
2020-08-31 14:44:50 -0500

    vistas

--- routes/index/index.js
+++ routes/index/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res,next)=>{
-    res.render('layout',{title: "login"})
+    res.render('layout',{type: "login"})
 })
 
 module.exports = router
--- routes/register/registerView.js
+++ routes/register/registerView.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res,next)=>{
-    res.render("layout",{title:"Register"})
+    res.render("layout",{type:"Register"})
 })
 
 
