efd1e5924676d067ff69c6ef8252ea54e986ee9f
chosoft <creadorcuentasmolina@gmail.com>
2020-09-01 22:45:35 -0500

    fin vistas de login y registro

--- public/js/register.js
+++ public/js/register.js
@@ -1,26 +1,8 @@
-$(document).ready(function() {
-    $("#submit").click(function(e){
+$(document).ready(function(){
+    $('.button-toggle').click(function(e){
         e.preventDefault()
-        let userName = $('#user').val()
-        let password = $('#password').val()
-        if(userName !== '' && password !== ''){
-            const data = {
-                userName,
-                password
-            }
-            axios({
-                url: '/api/login',
-                method: 'POST',
-                data: data
-            }).then(ok =>{
-                console.log(ok.data)
-            }).catch(err => console.log(err))
-        }else{
-            console.log('error')
-        }
-
+        console.log($(this).attr('ref'))
     })
-
     $('.button-toggle').click(function(e){
         e.preventDefault()
         let btn = $(this)
