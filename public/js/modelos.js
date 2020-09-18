31bc7952f246811c25557d2e7c4a5ee493216c52
chosoft <creadorcuentasmolina@gmail.com>
2020-09-18 10:08:44 -0500

    ok

--- public/js/modelos.js
+++ public/js/modelos.js
@@ -55,23 +55,84 @@ $(document).ready(function(){
                 url: '/api/modeladd',
                 method: 'POST',
                 data,
-            }).then(data => {
-                console.log(data)
+            }).then(ok => {
+               if(ok.data === 'fail'){
+                Swal.fire({
+                    title: "Error",
+                    text: "Ha ocurrido un error inesperado",
+                    icon: "error",
+                    confirmButtonText: "Ok"
+                })
+               }
+               else{
+                Swal.fire({
+                    title: "Bien hecho",
+                    text: "Se ha añadido el modelo correctamente",
+                    icon: "success",
+                    confirmButtonText: "Ok"
+                })
                 getterModels()
+               }
             }).catch(e => {
-                console.log(e)
+                Swal.fire({
+                    title: "Error",
+                    text: "Ha ocurrido un error inesperado",
+                    icon: "error",
+                    confirmButtonText: "Ok"
+                })
             })
         })
     })
-
-
     function getterModels(){
         axios({
             url: '/modelos',
             method: 'POST',
             data: true
-        }).then(data => {
-            console.log(data)
+        }).then(ok => {
+            if(Array.isArray(ok.data) && ok.data.length > 0){
+                $('#main').empty()
+                console.log('ok')
+                let template = ''
+                let template2 = ''
+                $('#main').innerHTML = `
+                    <div class="title-modelos">
+                        <h1>Modelos</h1>
+                    </div>
+                    <div class="wp-modelos" id="md">
+                    </div>
+
+                `
+                ok.data.forEach(dato => {
+                    template2 += `             
+
+                    <div class="card" idModelo="${dato._id}">
+                    <div class="card-header">
+                        <h2>${dato.nombre}</h2>
+                    </div>
+                    <div class="card-img"><img src="${dato.img}" alt="" srcset=""/></div>
+                    <div class="card-btns"><a class="btn link-btn" href="/modelos/${dato.nombre}">Ver ahora</a>
+                        <button class="btn btn-delete" deleterKey="${dato._id}">Eliminar</button>
+                    </div>
+                    </div>
+                    
+                    `
+                })
+                $('#md').innerHTML = template2
+            }else{
+                let template = `
+                                    <div class="modelos" id="empty">
+                    <div class="modelos-title">
+                        <h1>Modelos</h1>
+                    </div>
+                    <div class="modelos-p">
+                        <p>Aun no tines modelos agregados agrega uno dando click aqui abajo</p>
+                    </div>
+                    <div class="modelos-btn"><button id="add-model">Añadir proyecto</button></div>
+                    <div class="modelos-img"><img src="icons/empty.svg" alt="empty-draw" /></div>
+                </div>
+                `
+                $('#main').innerHTML = template
+            }
         }).catch(e => {
             console.log(e)
         })
